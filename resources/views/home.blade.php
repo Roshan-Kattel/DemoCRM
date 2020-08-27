944b208785b43a41c383792f171d633c25ad2f10
RoshanKattel <kattelroshan@gmail.com>
2020-08-27 18:07:46 +0545

    dashboard update

---
+++ resources/views/home.blade.php
@@ -0,0 +1,25 @@
+@extends('layouts.app')
+
+@section('content')
+<div class="container">
+    <div class="row justify-content-center">
+        <div class="col-md-8">
+            <div class="card">
+                <div class="card-header">{{ __('Dashboard') }}</div>
+
+                <div class="card-body">
+                    @if (session('status'))
+                        <div class="alert alert-success" role="alert">
+                            {{ session('status') }}
+                        </div>
+                    @endif
+                    {{ __('Please Choose one of the following:-') }}<br> 
+                    <a href="{{ url('account/create') }}">Account Openinig form</a><br>                    
+                    <a href="{{ url('cheque/create') }}">Cheque Book request</a><br>    
+                    <a href="{{ url('debit/create') }}">Debit Card/Internet Banking request</a><br>       
+                </div>
+            </div>
+        </div>
+    </div>
+</div>
+@endsection
